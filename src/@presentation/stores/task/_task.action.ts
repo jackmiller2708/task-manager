fa4ed347e9c6b52638487d9e592fe4bb2a636269
jackmiller0899 <jackmiller0899@gmail.com>
2024-06-12 07:25:50 +0000

    Lint Update

--- src/@presentation/stores/task/_task.action.ts
+++ src/@presentation/stores/task/_task.action.ts
@@ -1,16 +1,16 @@
-import type { List, Map as ImmutableMap } from 'immutable';
-import type { ITask } from '@application/models';
+import type { List, Map as ImmutableMap } from "immutable";
+import type { ITask } from "@application/models";
 
-import { createActionGroup, props } from '@ngrx/store';
+import { createActionGroup, props } from "@ngrx/store";
 
 export const TaskActions = createActionGroup({
-  source: 'Tasks',
+  source: "Tasks",
   events: {
-    'Populate Tasks': props<Readonly<{ tasks: ImmutableMap<string, ITask> }>>(),
-    'Select Tasks': props<Readonly<{ tasks: List<ITask> }>>(),
-    'Add Task': props<Readonly<{ task: ITask }>>(),
-    'Modify Task': props<Readonly<{ task: ITask }>>(),
-    'Delete Task': props<Readonly<{ id: string }>>(),
-    'Bulk Delete Task': props<Readonly<{ taskIds: List<string> }>>()
+    "Populate Tasks": props<Readonly<{ tasks: ImmutableMap<string, ITask> }>>(),
+    "Select Tasks": props<Readonly<{ tasks: List<ITask> }>>(),
+    "Add Task": props<Readonly<{ task: ITask }>>(),
+    "Modify Task": props<Readonly<{ task: ITask }>>(),
+    "Delete Task": props<Readonly<{ id: string }>>(),
+    "Bulk Delete Task": props<Readonly<{ taskIds: List<string> }>>(),
   },
 });
