93a3cdef33773512d89306d8ada06040cd6c216a
Jack Miller <jackmiller0899@gmail.com>
2024-06-15 11:20:09 +0700

    Cleanup config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,12 @@ import type { ApplicationConfig } from "@angular/core";
 import { provideLuxonDateAdapter } from "@angular/material-luxon-adapter";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideDatabase } from "@core/providers";
-import { provideRouter } from "@angular/router";
 import { tasksReducer } from "@presentation/stores";
 import { provideStore } from "@ngrx/store";
 import { schemas } from "@application/models/_schema";
-import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideStore({ tasks: tasksReducer }),
     provideDatabase(schemas),
     provideAnimations(),
